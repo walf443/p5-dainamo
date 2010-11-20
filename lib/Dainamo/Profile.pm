f7733cc9afd1d4121bc8b7c517f33e1117678d96
Keiji, Yoshimi <walf443@gmail.com>
2010-11-20 17:27:45 +0900

    user can set name.

--- lib/Dainamo/Profile.pm
+++ lib/Dainamo/Profile.pm
@@ -1,10 +1,17 @@
 package Dainamo::Profile;
 use strict;
 use warnings;
+use overload '""' => \&inspect;
 
 sub new {
     my ($class, %args) = @_;
     bless \%args, $class;
+}
+
+sub inspect {
+    my ($self, ) = @_;
+    my $class = ref $self;
+    return $self->{name} ? "$class\[@{[ $self->{name} ]}]" : $class;
 }
 
 sub run {
