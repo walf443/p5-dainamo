c1d3d33ea577b9eefca22a61b472d71fb8dd2ba0
Keiji, Yoshimi <walf443@gmail.com>
2010-11-21 19:49:43 +0900

    added startup script.

---
+++ lib/Dainamo/Util.pm
@@ -0,0 +1,30 @@
+package  Dainamo::Util;
+use strict;
+use warnings;
+
+# this code copied from Plack::Util::_load_sandbox.
+sub _load_sandbox {
+    my $_file = shift;
+
+    my $_package = $_file;
+    $_package =~ s/([^A-Za-z0-9_])/sprintf("_%2x", unpack("C", $1))/eg;
+
+    return eval sprintf <<'END_EVAL', $_package;
+package Dainamo::Sandbox::%s;
+{
+    my $app = do $_file;
+    if ( !$app && ( my $error = $@ || $! )) { die $error; }
+    $app;
+}
+END_EVAL
+}
+
+sub load {
+    my $file = shift;
+    my $dainamo = _load_sandbox($file);
+    die "Error while loading $file: $@" if $@;
+
+    return $dainamo;
+}
+
+1;
