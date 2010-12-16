c89d9e3b6cbcc19c8e271249157a4e8710cf7150
Keiji, Yoshimi <walf443@gmail.com>
2010-12-16 16:08:57 +0900

    change infof to debugf.

--- lib/Dainamo/Profile/Qudo.pm
+++ lib/Dainamo/Profile/Qudo.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use parent 'Dainamo::Profile';
 use Qudo;
-use Log::Minimal qw/infof/;
+use Log::Minimal qw/debugf/;
 
 sub new {
     my ($class, %args) = @_;
@@ -22,14 +22,16 @@ sub qudo {
 sub run {
     my ($self, ) = @_;
 
-    infof("start Dainamo::Profile::Qudo#run()");
+    debugf("start Dainamo::Profile::Qudo#run()");
     # copied from Qudo's work.
     my $work_delay = $self->qudo->{work_delay};
     unless ( $self->qudo->manager->work_once ) {
         $self->{qudo} = undef; # disconnect while sleep.
+        debugf("sleep Dainamo::Profile::Qudo#run()");
         sleep $work_delay;
     }
     $self->{qudo} = undef;
+    debugf("finish Dainamo::Profile::Qudo#run()");
 }
 
 1;
