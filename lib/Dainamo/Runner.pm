0d5379400dcf1c09d7318e894bba9cb533cd4b54
Keiji, Yoshimi <walf443@gmail.com>
2010-12-17 12:31:57 +0900

    use help message.

--- lib/Dainamo/Runner.pm
+++ lib/Dainamo/Runner.pm
@@ -15,12 +15,12 @@ sub parse_option {
 
     local @ARGV = @_;
 
-    opts my $max_workers => { isa => 'Int'},
-        my $daemonize => { isa => 'Bool', default => 0, },
-        my $reload => { isa => 'Bool' },
-        my $log_path => { isa => 'Str', },
-        my $log_level => { isa => 'Str' },
-        my $watch_path => { isa => 'Str' }, # FIXME: ほんとはPlack::Runnerにそろえたい(Reload)が、opts.pmの制限っぽい
+    opts my $max_workers => { isa => 'Int', comment => 'total process number of workers.'},
+        my $daemonize => { isa => 'Bool', default => 0, comment => 'do daemonize?' },
+        my $reload => { isa => 'Bool' , comment => 'do reload if $watcher files is changed' },
+        my $log_path => { isa => 'Str', comment => 'output path to log. you can also pipe with starting from "| ...".  and you can output $stderr when you specify STDERR.' },
+        my $log_level => { isa => 'Str', comment => 'specify log level. (info|debug)' },
+        my $watch_path => { isa => 'Str', comment => 'watching changing $watch_path and reload if $reload.' }, # FIXME: ほんとはPlack::Runnerにそろえたい(Reload)が、opts.pmの制限っぽい
         my $config => { isa => 'Str', required => 1 };
 
     $self->{max_workers} = $max_workers;
