76a793e802d656a1734bf07a211051ca37bcbc87
Keiji, Yoshimi <walf443@gmail.com>
2010-12-27 14:29:41 +0900

    update 0.05.

--- lib/Dainamo.pm
+++ lib/Dainamo.pm
@@ -11,7 +11,7 @@ use Plack::Handler::HTTP::Server::PSGI;
 use Plack::Builder;
 use Dainamo::Util;
 use Encode;
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 use 5.00800;
 
 has 'max_workers' => (
